912e859011ed36ef5fd6f4ff4e6ce46e60d7a9b8
adam-ghafara <adam.ghafara.job@gmail.com>
2024-02-05 13:27:28 +0700

    Modif Readme dan main.go

--- main/main.go
+++ main/main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
 	gisgolang "github.com/adam-ghafara/gis-baru"
 )
 
-func HelloHTTP(w http.ResponseWriter, r *http.Request) {
+func GeoIntersects(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodOptions {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE")
@@ -21,11 +20,85 @@ func HelloHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, gisgolang.PostGeoIntersects("MONGOSTRING", "geojsonList", "GISDB", r))
 }
 
-func handlerRequests() {
-	http.HandleFunc("/", HelloHTTP)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+func GeoWithin(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization,Token")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	fmt.Fprintf(w, gisgolang.PostGeoWithin("MONGOSTRING", "geojsonList", "GISDB", r))
+}
+
+func GeoNear(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization,Token")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	fmt.Fprintf(w, gisgolang.PostNear("MONGOSTRING", "geojsonList", "GISDB", r))
+}
+
+func NearSphere(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization,Token")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	fmt.Fprintf(w, gisgolang.PostNearSphere("MONGOSTRING", "geojsonList", "GISDB", r))
+}
+
+func GeoBox(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization,Token")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	fmt.Fprintf(w, gisgolang.PostBox("MONGOSTRING", "geojsonList", "GISDB", r))
+}
+
+func Center(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization,Token")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	fmt.Fprintf(w, gisgolang.PostCenter("MONGOSTRING", "geojsonList", "GISDB", r))
 }
 
 func main() {
-	handlerRequests()
+	http.HandleFunc("/geoIntersects", GeoIntersects)
+	http.HandleFunc("/geoWithin", GeoWithin)
+	http.HandleFunc("/geoNear", GeoNear)
+	http.HandleFunc("/nearSphere", NearSphere)
+	http.HandleFunc("/geoBox", GeoBox)
+	http.ListenAndServe(":8080", nil)
 }
+
+// func handlerRequests() {
+// 	http.HandleFunc("/", HelloHTTP)
+// 	log.Fatal(http.ListenAndServe(":8080", nil))
+// }
+
+// func main() {
+// 	handlerRequests()
+// }
